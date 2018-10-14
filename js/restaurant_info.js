b8bc0ffd2c141d12002ba639a59156204e1226c1
trmatt <39041501+trmatt@users.noreply.github.com>
2018-10-14 17:32:03 -0500

    Modifies the image alt atrribute of web pages

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -90,7 +90,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
-  addImageAlt(image);
+  // set alt atribute for images
+  image.alt = `A picture of ${restaurant.name} restaurant`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
@@ -154,7 +155,7 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.tabIndex='6';
-  
+
   name.innerHTML = review.name;
   li.appendChild(name);
 
@@ -198,55 +199,3 @@ getParameterByName = (name, url) => {
     return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
-
-
-/**
- * Adds descriptions to images
- */
-addImageAlt = (image) => {
-  switch(image.src) {
-    case 'http://localhost:8000/img/1.jpg':
-        image.alt = "a picture of people at Mission Chinese Food Restaurant";
-        break;
-
-    case 'http://localhost:8000/img/2.jpg':
-        image.alt = "a picture of a pizza on a dish at Emily's Restaurant";
-        break;
-
-    case 'http://localhost:8000/img/3.jpg':
-        image.alt = "showing inside Kang Ho Dong Baekjeong Restaurant";
-        break;
-
-    case 'http://localhost:8000/img/4.jpg':
-        image.alt = "showing outside Katz's Delicatessen";
-        break;
-
-    case 'http://localhost:8000/img/5.jpg':
-        image.alt = "a picture showing inside Roberta's Pizza Restaurant";
-        break;
-
-    case 'http://localhost:8000/img/6.jpg':
-        image.alt = "inside Hometown BBQ Restaurant";
-        break;
-
-    case 'http://localhost:8000/img/7.jpg':
-        image.alt = "showing people outside Superiority Burger Restaurant";
-        break;
-
-    case 'http://localhost:8000/img/8.jpg':
-        image.alt = "showing outside of The Dutch Restaurant";
-        break;
-
-    case 'http://localhost:8000/img/9.jpg':
-        image.alt = "showing people eating at MU Ramen Restaurant";
-        break;
-
-    case 'http://localhost:8000/img/10.jpg':
-        image.alt = "showing the decor inside Casa Enqique Restaurant";
-        break;
-
-    default:
-        image.alt = 'a picture';
-  }
-}
-
